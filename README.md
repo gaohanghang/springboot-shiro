3b1082f8b8f36f8a7b5165b27e6b3ffb2f969987
gaohanghang <1341947277@qq.com>
2019-01-22 15:51:08 +0800

    <文档改动|文件修改>: README添加描述，database.sql文件修改

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 > 项目来自[Springboot+Shiro 实现认证和授权](https://www.bilibili.com/video/av37984279/?p=12)
 
 
+### 功能
+
+- 认证(登录)功能
+- 授权功能
+- Thymeleaf整合权限标签，登录后只显示拥有有权限的功能
+
+
+
+![](https://ws1.sinaimg.cn/large/006tNc79ly1fzfei1tqoqj30g60b8wer.jpg)
+![](https://ws3.sinaimg.cn/large/006tNc79ly1fzfekz1gsdj30fn09hglx.jpg)
+*test1用户登录后只有用户添加功能显示*
+
+
+![](https://ws2.sinaimg.cn/large/006tNc79ly1fzfejfrt5vj30f40alwep.jpg)
+![](https://ws4.sinaimg.cn/large/006tNc79ly1fzfej3y9woj30d609h3yt.jpg)
+
+*test2用户登录后只有用户更新功能显示*
--- database.sql
+++ database.sql
@@ -1,6 +1,40 @@
--- 创建用户表
-CREATE TABLE user(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(20),
-	password VARCHAR(50)
-);
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : localhost:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : 65001
+
+ Date: 22/01/2019 15:33:52
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `perms` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES (1, 'test1', '123456', 'user:add');
+INSERT INTO `user` VALUES (2, 'test2', '123456', 'user:update');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
